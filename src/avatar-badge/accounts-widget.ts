02e2659f27aba6b6ff618e7930e27f1f977a775d
Nikita Teremovskiy <ni-2@mail.ru>
2022-11-30 18:27:21 +0300

    refactor: extract resources to separate module from accounts-widget

--- src/avatar-badge/accounts-widget.ts
+++ src/avatar-badge/accounts-widget.ts
@@ -5,17 +5,13 @@ import { styleMap } from 'lit/directives/style-map.js'
 import { styles } from './accounts-widget.css'
 import COPIED from './assets/copied.svg'
 import COPY from './assets/copy.svg'
-import GITHUB_ICON from './assets/github.svg'
-import NEAR_ICON from './assets/near-black.svg'
-import TWITTER_ICON from './assets/twitter-icon.svg'
+import { resources } from './resources'
 import { IConnectedAccountUser } from './types'
 
 export interface IAccountsWidgetState {
   username: string
   accounts: IConnectedAccountUser[]
   showAccounts: boolean
-  el: HTMLElement
-  insPointName: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   handleClose: any
 }
@@ -23,36 +19,15 @@ export interface IAccountsWidgetState {
 export class AccountsWidget extends LitElement implements IAccountsWidgetState {
   public static override styles = styles
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @property() state: any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @property() ctx: any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @property() init: any
   @property() username: string
   @property() accounts: IConnectedAccountUser[]
   @property() showAccounts: boolean
-  @property() el: HTMLElement
-  @property() insPointName: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   @property() handleClose: any
 
-  connectedCallback() {
-    super.connectedCallback()
-    this.init?.(this.ctx, this.state)
-  }
-
   private _clickAccountHandler = (account: IConnectedAccountUser) => (e) => {
     e.preventDefault()
-    if (account.origin === 'twitter') {
-      window.open(`https://twitter.com/${account.name}`, '_blank')
-    } else if (account.origin === 'github') {
-      window.open(`https://github.com/${account.name}`, '_blank')
-    } else if (account.origin === 'near/testnet') {
-      window.open(`https://explorer.testnet.near.org/accounts/${account.name}`, '_blank')
-    } else if (account.origin === 'near/mainnet') {
-      window.open(`https://explorer.near.org/accounts/${account.name}`, '_blank')
-    }
+    window.open(resources[account.origin].uri(account.name), '_blank')
     this.handleClose()
   }
 
@@ -86,22 +61,10 @@ export class AccountsWidget extends LitElement implements IAccountsWidgetState {
             html`<div class="account-container">
               <div
                 class=${classMap({ account: true, nameUserActive: account.accountActive })}
-                title=${'Go to the ' +
-                (account.origin === 'twitter'
-                  ? 'Twitter page'
-                  : account.origin === 'github'
-                  ? 'GitHub page'
-                  : 'NEAR explorer')}
+                title=${'Go to the ' + resources[account.origin].pageName}
                 @click=${this._clickAccountHandler(account)}
               >
-                <img
-                  src=${account.origin === 'twitter'
-                    ? TWITTER_ICON
-                    : account.origin === 'github'
-                    ? GITHUB_ICON
-                    : NEAR_ICON}
-                  class="imgUser"
-                />
+                <img src=${resources[account.origin].icon} class="imgUser" />
                 <h4 class="nameUser">${account.name}</h4>
               </div>
               <a class="copy-button" title="copy ID" @click=${this._clickCopyHandler(account)}>
